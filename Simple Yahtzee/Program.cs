ebc2295f20cd2e04054ea77456e9ebc0d6aa0187
Jennifer Young <jclyoung@gmail.com>
2019-03-19 21:26:06 -0500

    Almost there

--- Simple Yahtzee/Program.cs
+++ Simple Yahtzee/Program.cs
@@ -20,25 +20,28 @@ namespace Simple_Yahtzee
             Console.WriteLine("1) Play Yahtzee");
             Console.WriteLine("2) Instructions");
             Console.WriteLine("3) Quit");
-            int userSelection = Console.Read());
-            if (userSelection == 1 )
+            string userSelection = Console.ReadLine();
+            if (userSelection == "1" )
             {
                 Yahtzee();
                 return true;
             }
-            else if (userSelection == 2)
+            else if (userSelection == "2")
             {
                 Instructions();
                 return true;
             }
-            else if (userSelection == 3)
+            else if (userSelection == "3")
                 return false;
             return true;
 
         }
 
+        /// <summary>
+        /// Instructions for playing the game.
+        /// </summary>
         public static void Instructions()
-        {   //break sentences to make it more asthetically pleasing
+        {   
 
             Console.WriteLine("The object of this game is to get a 2, 3, 4 or 5 of a kind." +
                 "/EEverytime you roll the dice you will be able to select which dice you would like to keep." +
@@ -47,17 +50,11 @@ namespace Simple_Yahtzee
             Console.WriteLine("1) Play Yahtzee");
             Console.WriteLine("2) Main Menu");
             Console.WriteLine("3) Quit");
-            int userSelection = Console.Read());
+            int userSelection = Console.Read();
             if (userSelection == 1)
-            {
                 Yahtzee();
-
-            }
             else if (userSelection == 2)
-            {
-                MainMenu();
-
-            }
+                MainMenu();            
             else if (userSelection == 3)
                 return;
         }
@@ -71,17 +68,27 @@ namespace Simple_Yahtzee
             Console.WriteLine("Player One, please enter your name");
             string playerOne = Console.ReadLine();
             Console.WriteLine($"{playerOne} press enter to roll the dice.");
+            Console.WriteLine();
             Console.ReadLine();
-            RollTheDice(keptDice);
+            int[] rolledDice = RollTheDice(keptDice);
+            foreach (int roll in rolledDice)
+            {
+                Console.Write(roll);
+                Console.Write(", ");
+            }
 
-                
-                
-                //make into string as otherwise it only comes up with the type of array it is
+            //make into string as otherwise it only comes up with the type of array it is
+            Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Please select the die you would like to keep. " +
                 "If there is more than one die then seperate each number with a \",\""
                 + " ie. if the dice came up 4, 6, 2, 1, 2. You can enter 2,2 for what you would like to keep ");
             string userInput = Console.ReadLine();
+
+            
+
+        
+             
 
             //Look up how to convert string into an int (Because you forgot :(  )
             // Look up how to use split and join. If statements will allow you to see if their numbers are correct or not. 
@@ -89,6 +96,7 @@ namespace Simple_Yahtzee
             //
 
         }
+
         public static int[] RollTheDice(int keptDice)
         {
             int numberOfDice = 5 - keptDice;
@@ -101,6 +109,20 @@ namespace Simple_Yahtzee
             return randomRoll;
         }
 
+        public static int ChoosenDie()
+        {
+
+
+
+        }
+
+
+
+
     }
+
+
+
+
 
 }

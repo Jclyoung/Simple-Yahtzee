53ad2416ce4322b5a85f01d4ba9d213805a72a4e
Jennifer Young <jclyoung@gmail.com>
2019-03-24 16:38:12 -0500

    Help!

--- Simple Yahtzee/Program.cs
+++ Simple Yahtzee/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 
 
+
 namespace Simple_Yahtzee
 {
     class Program
-    {
+    {       
         public static void Main(string[] args)
-        { //myarray.cacant.newarray.toarray
-            bool displayMenu = true;
-            while (displayMenu)
-            {
-                displayMenu = MainMenu();
-            }
+        { //myarray.concat.newarray.toarray
+            GameLoop();
         }
+
         public static bool MainMenu()
         {
             Console.Clear();
@@ -34,79 +32,134 @@ namespace Simple_Yahtzee
             else if (userSelection == "3")
                 return false;
             return true;
-
         }
 
-        /// <summary>
-        /// Instructions for playing the game.
-        /// </summary>
-        public static void Instructions()
-        {   
+        public static void GameLoop()
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
 
-            Console.WriteLine("The object of this game is to get a 2, 3, 4 or 5 of a kind." +
-                "/EEverytime you roll the dice you will be able to select which dice you would like to keep." +
-                "/YYou will get a total of 3 rolls.");
-            Console.WriteLine("Choose an Option:");
-            Console.WriteLine("1) Play Yahtzee");
-            Console.WriteLine("2) Main Menu");
-            Console.WriteLine("3) Quit");
-            int userSelection = Console.Read();
-            if (userSelection == 1)
-                Yahtzee();
-            else if (userSelection == 2)
-                MainMenu();            
-            else if (userSelection == 3)
-                return;
+        public static void Instructions()
+        {
+            Console.Clear();
+            Console.WriteLine("" +
+                            "\nObjective - The object of Simple Yahtzee is to obtain a score from throwing 5 dice" +
+                            "\nThe game consists of 1 round, which contains a maximum of three rolls. To start" +
+                            "\nwith, roll all the dice. After rolling you can either score the current roll " +
+                            "\n(see below), or re-roll any or all of the dice. You may only roll the dice a total" +
+                            "\nof 3 times. After rolling 3 times your score will be calculated. If you choose" +
+                            "\nto keep all dice before the 3rd roll, your score wil be calculated then." +
+                            "\ni.e. it doesn't have to be after the 3rd roll." +
+                            "\n" +
+                            "\nScoring - To gain points you must obtain one of the following:" +
+                            "\n3 of a kind - 3 of a kind you must have at least 3 of the same die faces." +
+                            "\n4 of a kind - 4 of a kind you must have at least 4 of the same die faces." +
+                            "\nFull House - Full House you must have a 3 of a kind and 2 of a kind." +
+                            "\nYahtzee - Yahtzee you must have at least 5 of the same die faces." +
+                            "\n" +
+                            "\nPoints are calculated as follows:" +
+                            "\n2 of a kind  = 2 points total" +
+                            "\n3 of a kind  = 3 points total" +
+                            "\n4 of a kind  = 4 points total" +
+                            "\nFull House   = 25 Points total" +
+                            "\nYahtzee      = 50 points total"); 
+
+         Console.WriteLine("Choose an Option:" +
+                           "\n1) Play Yahtzee" +
+                           "\n2) Main Menu");
+            string userSelection = Console.ReadLine();
+            if (userSelection == "1")
+               Yahtzee();
+            else if (userSelection == "2")
+               MainMenu();
+            return;
         }
        
         public static void Yahtzee()
         {
             Console.Clear();
             int keptDice = 0;
-            int userSelection = 0;
-            Console.WriteLine("Welcome to a basic game of Yahtzee dice.");
-            Console.WriteLine("Player One, please enter your name");
-            string playerOne = Console.ReadLine();
-            Console.WriteLine($"{playerOne} press enter to roll the dice.");
-            Console.WriteLine();
+           
+            Console.WriteLine("Welcome to a basic game of Yahtzee dice." +
+                "\n" +              
+                "\nPlayer One, please enter your name");
+            string playerName = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"{playerName} press enter to roll the dice.");
             Console.ReadLine();
-            int[] rolledDice = RollTheDice(keptDice);
-            foreach (int roll in rolledDice)
+            
+            string[] firstRoll = RollTheDice(keptDice);
+            UserSelectsDice(firstRoll);
+            keptDice = Reroll(firstRoll);
+            if (keptDice > 0)
             {
-                Console.Write(roll);
-                Console.Write(", ");
-            }            
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Please select the die you would like to keep. " +
-                "If there is more than one die then seperate each number with a \",\""
-                + " ie. if the dice came up 4, 6, 2, 1, 2. You can enter 2,2 for what you would like to keep ");
-            string userInput = Console.ReadLine();       
-                
-            // Look up how to use split and join. If statements will allow you to see if their numbers are correct or not. 
-            //get the kept number of dice by calculating the length of the array the user made.
-            //
-
+                string[] secondRoll = RollTheDice(keptDice);
+                UserSelectsDice(secondRoll);
+            }
+            if (keptDice > 0)
+            {
+                string[] thirdRoll = RollTheDice(keptDice);
+                UserSelectsDice(thirdRoll);               
+            }
+            TallyScore();
         }
-
-        public static int[] RollTheDice(int keptDice)
+        
+        public static string[] RollTheDice(int keptDice)
         {
             int numberOfDice = 5 - keptDice;
-            int[] randomRoll = new int[numberOfDice];
+            string[] randomRoll = new string[numberOfDice];
             for (int i = 0; i < randomRoll.Length; i++)
             {
                 int die = new Random().Next(1, 7);
-                randomRoll[i] = die;
+                randomRoll[i] = die.ToString();
             }
             return randomRoll;
         }
 
-        public static int ChoosenDie(userInput)
+        public static string[] UserSelectsDice(string[] rollDice)
         {
 
+            int diceId = 0;
+            int diceIndex = 0;
+            Console.WriteLine("You rolled: \n" +
+                string.Join(", ", rollDice) +
+                "\n\nPress enter to continue.");
+            Console.ReadLine();
+            foreach (string roll in rollDice)
+            {
+                diceId++;
+                Console.WriteLine();
+                Console.WriteLine($"{diceId}) {roll}");
+                Console.WriteLine("If you would like to keep this die, type the letter \"y\" and then enter." +
+                                  "\nOtherwise, press enter to continue");
 
-
+                string userSelection = Console.ReadLine();
+                if (userSelection != "Y" || userSelection != "y")
+                {
+                    rollDice[diceIndex] = string.Empty;
+                    diceIndex++;
+                }
+            }
+            return rollDice;
         }
+
+        private static int Reroll(string[] userSelectedDice)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void TallyScore()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
 
 
 
